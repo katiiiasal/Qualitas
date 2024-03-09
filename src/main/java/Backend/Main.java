bd08bbeef73cf9ecec7f7f2bd765d86b26673457
katiiiasal <2123100501@soy.utj.edu.mx>
2024-03-09 14:17:54 -0600

    Se modifico layout para que este mas asthetic

---
+++ src/main/java/Backend/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Backend;
+
+/**
+ *
+ * @author heiva
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Amo Seshomaru");
+    }
+}
