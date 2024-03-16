3a4e46796bfdb11f1c2f596fb39562ea7e5d99c3
katiiiasal <2123100501@soy.utj.edu.mx>
2024-03-16 15:39:18 -0600

    Se agregaron los atributos a la VistaClientes.java

--- src/main/java/Frontend/VistaClientes.java
+++ src/main/java/Frontend/VistaClientes.java
@@ -52,15 +52,22 @@ public class VistaClientes extends javax.swing.JFrame {
                 {null, null, null, null, null, null, null}
             },
             new String [] {
-                "idProducto", "Nombre", "Descripcion", "Lote", "Fecha produccion", "Fecha Expiracion", "Alerta Expiracion"
+                "idCliente", "nombre", "apellidoMaterno", "apellidoPaterno", "direccion", "email", "telefono"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Object.class, java.lang.Object.class, java.lang.Boolean.class
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         spHeadersTable.setViewportView(tblProductos);
